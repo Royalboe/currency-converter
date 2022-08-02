5467236d4a1ec0d582b3acf656db317a7da05caf
Royalboe <auomidejohnson@gmail.com>
2022-08-02 08:10:34 +0100

    Added props to the app component

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 
 function App() {
-  const [amount1, setAmount1] = useState(1);
-  const [amount2, setAmount2] = useState(1);
-  const [currency1, setCurrency1] = useState("USD");
-  const [currency2, setCurrency2] = useState("USD");
+  const [amount1, setAmountOne] = useState(1);
+  const [amount2, setAmountTwo] = useState(1);
+  const [currency1, setCurrencyOne] = useState("USD");
+  const [currency2, setCurrencyTwo] = useState("USD");
   const [rates, getRates] = useState([]);
 
   useEffect(() => {
@@ -23,11 +23,15 @@ function App() {
   return (
     <div className="App">
       <CurrencyInput
+        onAmountChange={setAmountOne}
+        onCurrencyChange={setCurrencyOne}
         currencies={Object.keys(rates)}
         amount={amount1}
         currency={currency1}
       />
       <CurrencyInput
+        onCurrencyChange={setCurrencyTwo}
+        onAmountChange={setAmountTwo}
         currencies={Object.keys(rates)}
         amount={amount2}
         currency={currency2}
